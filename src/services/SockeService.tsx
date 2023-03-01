f1f669f50bc7ef92878727e11b81ecd420b10ad1
unknown <benjaminfkile@gmail.com>
2023-03-01 01:26:21 -0700

    kill logs

--- src/services/SockeService.tsx
+++ src/services/SockeService.tsx
@@ -3,7 +3,6 @@ const aboutService = require("./AboutService")
 const socketService = {
     intit: (io: any, db: any) => {
         io.on("connection", (socket: any) => {
-            console.log("a user connected")
             socket.on("send-about-JSON", () => {
                 aboutService.getAbout(db).then((about: AboutTypes) => {
                     io.emit("recieve-about-JSON", about)
