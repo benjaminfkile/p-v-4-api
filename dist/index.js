12a94a02ab4e8fbe9cea0812d4e7cebd8bd6b6d5
unknown <benjaminfkile@gmail.com>
2023-03-01 00:34:24 -0700

    switch to live dev db

--- dist/index.js
+++ dist/index.js
@@ -10,7 +10,7 @@ const db = knex({
     client: "pg",
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: process.env.DATABASE_URL ? false : true
+        ssl: { rejectUnauthorized: false }
     }
 });
 app.set("db", db);
